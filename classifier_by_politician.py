5a00d47da474008743e8fc88ba14307ac64d058d
Cooper Chia <crchia@uw.edu>
2020-05-28 11:24:11 -0700

    not much

--- classifier_by_politician.py
+++ classifier_by_politician.py
@@ -3,17 +3,16 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
-import numpy as np
 
 def main():
     df = pd.read_csv('ExtractedTweets.csv')
     grouped = df.groupby(["Handle", "Party"])["Tweet"].sum()
-    df = grouped.reset_index()
+    grouped = grouped.reset_index()
 
-    party = df.iloc[:, 1]
-    tweets = df.iloc[:, 2]
+    party = grouped.iloc[:, 1]
+    tweets = grouped.iloc[:, 2]
 
-    tweets_train, tweets_test, party_train, party_test = train_test_split(tweets, party, test_size=0.3)
+    tweets_train, tweets_test, party_train, party_test = train_test_split(tweets, party, test_size=0.25)
     vectorizer = CountVectorizer()
     counts = vectorizer.fit_transform(tweets_train.values)
 
