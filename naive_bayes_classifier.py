0de8b665b94b10122367438b94023ca53696bcb1
Walker Azam <65573149+walkerazam@users.noreply.github.com>
2020-06-04 12:05:26 -0700

    Update naive_bayes_classifier.py

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,3 +1,11 @@
+"""
+Evan Yip, Cooper Chia, Walker Azam
+CSE 163 Final Project (June/2020)
+
+This file contains methods to generate a Naive Bayes
+classifier to classify twitter users as Democrats
+or Republicans based off their tweets
+"""
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
@@ -5,45 +13,75 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 import pickle
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import seaborn as sns
 
 
 def group_data(df):
+    '''
+    This helper function groups together tweets from the same person
+    and by party. It takes in a dataframe of tweets as a parameter
+    and returns a grouped dataframe.
+    '''
     grouped = df.groupby(["Handle", "Party"])["Tweet"].sum()
     grouped = grouped.reset_index()
     return grouped
 
 
 def naive_bayes(data):
+    '''
+    This function takes a dataframe containing tweets and party affiliation
+    from an imported file. It generates 8 different naive bayes models
+    according to different test sizes and generates multiple plots to
+    visualise their effectiveness in prediction. It also saves a plot
+    comparing all model's accuracy scores in a plot called
+    accuracy_by_test_size.png.
+    '''
+    # seperating labels and tweets
     party = data.loc[:, "Party"]
     tweets = data.loc[:, "Tweet"]
-    accuracy_map = {}
+    accuracy_map = {}  # a dictionary to store the accuracy scores of the model
+
+    # loops through different test sample sizes to test against a trained model
     for i in range(8):
         test_size = round(0.25 + i * 0.1, 2)
+        # seperating testing and training data accorind to test size
         tweets_train, tweets_test, party_train,\
             party_test = train_test_split(tweets, party, test_size=test_size)
+        # generating a vectorizer to handle string inputs
         vectorizer = CountVectorizer()
+        # calling function to train model
         classifier = train_bayes(tweets_train, party_train, vectorizer)
         predictions = classifier.predict(vectorizer.transform(tweets_test))
         acc = accuracy_score(party_test, predictions)
-        accuracy_map[test_size] = acc
+        accuracy_map[test_size] = acc  # storing the accuracy score
+        # calling functions to visualise each model according to testing size
         matrix_display(party_test, predictions, test_size)
         plot_accuracy_bar(party_test, predictions, test_size)
+
+    # creating a new dataframe that stores accuracy scores
     accuracy_df = pd.DataFrame(list(accuracy_map.items()),
                                columns=["Test Size", "Accuracy"])
     print(accuracy_df)
+    # plotting accuracy scores of models by their test size
     sns.relplot(x="Test Size", y="Accuracy", data=accuracy_df)
     plt.title("Accuracy vs. Test Size")
     plt.savefig("accuracy_by_test_size.png")
 
 
 def train_bayes(tweets_train, party_train, vectorizer):
+    '''
+    This function trains a multinomial naive bayes classifier model.
+    It takes training data and training labers, and the vectorizer
+    to handle strings, as parameters. It returns a trained classifier
+    based on the training data/labels given.
+    '''
     counts = vectorizer.fit_transform(tweets_train.values)
     classifier = MultinomialNB()
     target = party_train.values
     classifier.fit(counts, target)
-    # Writing model to a pickle
+    # Writing model to a pickle to save it
     save_model(classifier, vectorizer)
     return classifier
 
@@ -64,10 +102,10 @@ def save_model(classifier, vectorizer):
 def matrix_display(party_test, predictions, test_size):
     '''
     This function takes true labels and the predictions from the data
-    used to test the naive bayes classifier, and creates a confusion
-    matrix showing the accuracy of the classifier.
+    used as parameters, to test the naive bayes classifier,
+    and creates a confusion matrix showing the accuracy of the classifier.
+    It saves the confusion matrix under a png file(no returns).
     '''
-    from sklearn.metrics import confusion_matrix
     party_labels = ['Democrat', 'Republican']
     c_matrix = confusion_matrix(party_test, predictions, party_labels)
 
